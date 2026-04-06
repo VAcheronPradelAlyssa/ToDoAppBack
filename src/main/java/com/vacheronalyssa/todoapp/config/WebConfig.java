e9c51082737e043725de022785713cc4f1b2838d
Alyssa Vacheron <vacheron.alyssa@aol.com>
2026-04-06 14:50:43 +0200

    feat: met à jour la configuration CORS pour autoriser plusieurs origines

--- src/main/java/com/vacheronalyssa/todoapp/config/WebConfig.java
+++ src/main/java/com/vacheronalyssa/todoapp/config/WebConfig.java
@@ -13,7 +13,12 @@ public class WebConfig {
             @Override
             public void addCorsMappings(CorsRegistry registry) {
                 registry.addMapping("/**")
-                        .allowedOrigins("http://localhost:4200")
+                    .allowedOriginPatterns(
+                        "http://localhost:4200",
+                        "https://to-do-app-front-eight.vercel.app",
+                        "https://to-do-app-front-*.vercel.app",
+                        "https://*.vercel.app"
+                    )
                         .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS")
                         .allowedHeaders("*")
                         .allowCredentials(true);
